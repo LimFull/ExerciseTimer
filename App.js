afe7afbce828589f88a2817cf494a52a9b34a117
LimFull <full7002@naver.com>
2021-05-29 15:24:49 +0900

    start project

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import './global';
+
+const App = () => {
+  return (
+    <View style={{width: '100%', height: '100%', backgroundColor: 'blue'}} />
+  );
+};
+
+export default App;

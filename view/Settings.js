c428178634312fdc26252b1a982a58611b08d3eb
LimFull <full7002@naver.com>
2021-06-05 19:38:47 +0900

    설정에 시간바꾸기 추가

--- view/Settings.js
+++ view/Settings.js
@@ -1,12 +1,94 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
 import {setTimes} from '@store';
 import {useSelector, useDispatch, connect} from 'react-redux';
+import {sensitiveHeaders} from '../node_modules_back/@types/node/http2.d';
 
 const Settings = () => {
+  const state = useSelector(state => state);
+  const {firstTime, secondTime, thirdTime} = useSelector(state => state);
+  const dispatch = useDispatch();
+  const getMinute = time => {
+    return parseInt(time / 60000);
+  };
+
+  const getSecond = time => {
+    return parseInt(time % 60000);
+  };
+
+  const setMinute = (target, time) => {
+    const m = time * 60000;
+    const s = getSecond(state[target]);
+    dispatch(setTimes({[target]: m + s}));
+  };
+
+  const setSecond = (target, time) => {
+    const m = getMinute(state[target]) * 60000;
+    const s = time * 1000;
+    dispatch(setTimes({[target]: m + s}));
+  };
+
+  const renderInput = (text, time, target, defaultValue) => {
+    const m = getMinute(time);
+    const s = getSecond(time);
+    return (
+      <View style={{marginBottom: 15, alignItems: 'center'}}>
+        <Text style={{fontSize: 20, marginBottom: 5}}>{text}</Text>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <TextInput
+            maxLength={2}
+            keyboardType={'decimal-pad'}
+            style={{
+              width: 50,
+              height: 50,
+              backgroundColor: 'white',
+              fontSize: 20,
+              marginBottom: 10,
+              marginLeft: 10,
+            }}
+            defaultValue={defaultValue.m.toString()}
+            textAlign="center"
+            onSubmitEditing={e => {
+              setMinute(target, Number(e.nativeEvent.text));
+            }}
+            placeholder={'00'}
+          />
+          <Text>분</Text>
+          <TextInput
+            maxLength={2}
+            keyboardType={'decimal-pad'}
+            style={{
+              width: 50,
+              height: 50,
+              backgroundColor: 'white',
+              fontSize: 20,
+              marginBottom: 10,
+              marginLeft: 10,
+            }}
+            defaultValue={defaultValue.s.toString()}
+            textAlign="center"
+            onSubmitEditing={e => {
+              setSecond(target, Number(e.nativeEvent.text));
+            }}
+            placeholder={'00'}
+          />
+          <Text>초</Text>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Settings</Text>
+      {renderInput('1번 휴식 시간', firstTime, 'firstTime', {m: 1, s: 30})}
+      {renderInput('2번 휴식 시간', secondTime, 'secondTime', {m: 3, s: 0})}
+      {renderInput('3번 휴식 시간', thirdTime, 'thirdTime', {m: 5, s: 0})}
     </View>
   );
 };
@@ -17,7 +99,6 @@ const styles = StyleSheet.create({
   container: {
     width: '100%',
     height: '100%',
-    backgroundColor: '#7FFFD4',
     alignItems: 'center',
     justifyContent: 'center',
   },
